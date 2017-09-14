82edd0b5522f0e6256d3378b17225cf79c4627c0
Semyeong Oh <semyeong.oh@gmail.com>
2017-09-14 17:52:23 -0400

    add submit script

---
+++ paper/submit.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+target_dir="submission"
+if [ -d "$target_dir" ]; then
+  echo "${target_dir} exists; do nothing"
+  exit 1;
+fi
+mkdir $target_dir
+
+list_of_figures=$(grep includegraphics ms.tex | sed 's/.*{\(.*\)}/figures\/\1/')
+echo "grabbing following figures\n";
+echo $list_of_figures;
+for figfile in $list_of_figures; do
+  cp $figfile $target_dir;
+done
+
+paperfiles=$(git ls-tree --name-only HEAD | grep -v ".gitignore\|Makefile\|figures|*.sh")
+for f in $paperfiles; do
+  echo "copying $f to $target_dir";
+  cp $f $target_dir;
+done
+
+echo "compress all files in $submission to $submission.tar.gz? [yn]"
+select yn in "y" "n"; do
+  case $yn in
+      Yes ) tar -czvf "$target_dir.tar.gz" $target_dir/*; break;;
+      No ) exit;;
+  esac
+done
